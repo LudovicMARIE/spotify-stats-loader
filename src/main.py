dc6e404904a4e6bb7c7853b4621d71d2e5bc7612
Ludovic MARIE <91457982+LudovicMARIE@users.noreply.github.com>
2025-08-30 16:50:43 +0200

    reworked date filters

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 from processor import *
 
 
-# unique_songs_df = get_unique_songs(2025, 6)
+# filtered_by_date_df = filter_by_date(2025, 6, 1)
+# print(filtered_by_date_df)
+
+# filtered_by_date_interval_default_df = filter_by_date_interval()
+# print(filtered_by_date_interval_default_df)
+
+# filtered_by_date_interval_df = filter_by_date_interval("2024-05-06", None)
+# print(filtered_by_date_interval_df)
+
+
+# unique_songs_df = get_unique_songs(filter_by_date(2024))
 # print(unique_songs_df)
 
 
-unique_artists_df = get_unique_artists(2021)
-print(unique_artists_df)
+# unique_artists_df = get_unique_artists()
+# print(unique_artists_df)
 
 
--- src/processor.py
+++ src/processor.py
@@ -10,10 +10,8 @@ df['played_at'] = df['ts'].apply(lambda t: arrow.get(t).datetime)
 sorted_df = df.sort_values(by="played_at")
 
 
-### Unique stats
-
-# Get song list with playcount, time listened, ordered by playcount
-def get_unique_songs(yearParam: int = None, monthParam: int = None, dayParam: int = None):
+# Filter df by year, month or/and day
+def filter_by_date(yearParam: int = None, monthParam: int = None, dayParam: int = None):
     filtered_df = sorted_df
 
     if yearParam is not None:
@@ -27,9 +25,31 @@ def get_unique_songs(yearParam: int = None, monthParam: int = None, dayParam: in
     if dayParam is not None:
         print("filtering day")
         filtered_df = filtered_df[filtered_df["played_at"].dt.day == dayParam]
-    
-    
-    song_stats = filtered_df.groupby(["master_metadata_track_name", "master_metadata_album_artist_name"]).agg(
+
+    return filtered_df.reset_index(drop=True)
+
+# Filter df between two dates
+def filter_by_date_interval(startDateParam: str = None, endDateParam: str = None):
+    if startDateParam is not None:
+        startDate = arrow.get(startDateParam)
+    else:
+        startDate = arrow.get("1975-01-01")
+
+    if endDateParam is not None:
+        endDate = arrow.get(endDateParam)
+    else:
+        endDate = arrow.get(arrow.utcnow())
+
+    filtered_df = sorted_df[(df["played_at"] >= startDate.datetime) &
+                            (df["played_at"] < endDate.datetime)]
+
+    return filtered_df.reset_index(drop=True)
+
+### Unique stats and playcount
+
+# Get song list with playcount, time listened, ordered by playcount
+def get_unique_songs(dfParam):
+    song_stats = dfParam.groupby(["master_metadata_track_name", "master_metadata_album_artist_name"]).agg(
         count=("ms_played", lambda x: (x >= 30000).sum()), # Song not counting if not played for at least 30.000 ms
         total_ms_played=("ms_played", "sum")
     )
@@ -39,24 +59,8 @@ def get_unique_songs(yearParam: int = None, monthParam: int = None, dayParam: in
     return song_stats
 
 # Get artists list with playcount, time listened, ordered by playcount
-def get_unique_artists(yearParam: int = None, monthParam: int = None, dayParam: int = None):
-    filtered_df = sorted_df
-
-    if yearParam is not None:
-        print("filtering year")
-        filtered_df = filtered_df[filtered_df["played_at"].dt.year == yearParam]
-
-    if monthParam is not None:
-        print("filtering month")
-        filtered_df = filtered_df[filtered_df["played_at"].dt.month == monthParam]
-        
-    if dayParam is not None:
-        print("filtering day")
-        filtered_df = filtered_df[filtered_df["played_at"].dt.day == dayParam]
-
-
-
-    artists_df = filtered_df.groupby(["master_metadata_album_artist_name"]).agg(
+def get_unique_artists(dfParam):
+    artists_df = dfParam.groupby(["master_metadata_album_artist_name"]).agg(
         count=("ms_played", lambda x: (x >= 30000).sum()),
         total_ms_played=("ms_played", "sum")
     )
@@ -64,3 +68,6 @@ def get_unique_artists(yearParam: int = None, monthParam: int = None, dayParam: 
     artists_df = artists_df.sort_values(by="count", ascending=False).reset_index()
 
     return artists_df
+
+# def get_unique_songs_by_artist():
+
