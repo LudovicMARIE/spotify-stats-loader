7ae8c99b8925028ccf51fc8617ad1576c652ab8b
Ludovic MARIE <91457982+LudovicMARIE@users.noreply.github.com>
2025-08-30 14:40:30 +0200

    added filters date to getSongs + getArtists

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from processor import *
 
 
-# unique_songs_df = get_unique_songs()
+# unique_songs_df = get_unique_songs(2025, 6)
 # print(unique_songs_df)
 
 
-unique_artists_df = get_unique_artists()
+unique_artists_df = get_unique_artists(2021)
 print(unique_artists_df)
 
 
--- src/processor.py
+++ src/processor.py
@@ -13,9 +13,23 @@ sorted_df = df.sort_values(by="played_at")
 ### Unique stats
 
 # Get song list with playcount, time listened, ordered by playcount
-def get_unique_songs():
+def get_unique_songs(yearParam: int = None, monthParam: int = None, dayParam: int = None):
+    filtered_df = sorted_df
+
+    if yearParam is not None:
+        print("filtering year")
+        filtered_df = filtered_df[filtered_df["played_at"].dt.year == yearParam]
+
+    if monthParam is not None:
+        print("filtering month")
+        filtered_df = filtered_df[filtered_df["played_at"].dt.month == monthParam]
+        
+    if dayParam is not None:
+        print("filtering day")
+        filtered_df = filtered_df[filtered_df["played_at"].dt.day == dayParam]
     
-    song_stats = df.groupby(["master_metadata_track_name", "master_metadata_album_artist_name"]).agg(
+    
+    song_stats = filtered_df.groupby(["master_metadata_track_name", "master_metadata_album_artist_name"]).agg(
         count=("ms_played", lambda x: (x >= 30000).sum()), # Song not counting if not played for at least 30.000 ms
         total_ms_played=("ms_played", "sum")
     )
@@ -25,8 +39,24 @@ def get_unique_songs():
     return song_stats
 
 # Get artists list with playcount, time listened, ordered by playcount
-def get_unique_artists():
-    artists_df = df.groupby(["master_metadata_album_artist_name"]).agg(
+def get_unique_artists(yearParam: int = None, monthParam: int = None, dayParam: int = None):
+    filtered_df = sorted_df
+
+    if yearParam is not None:
+        print("filtering year")
+        filtered_df = filtered_df[filtered_df["played_at"].dt.year == yearParam]
+
+    if monthParam is not None:
+        print("filtering month")
+        filtered_df = filtered_df[filtered_df["played_at"].dt.month == monthParam]
+        
+    if dayParam is not None:
+        print("filtering day")
+        filtered_df = filtered_df[filtered_df["played_at"].dt.day == dayParam]
+
+
+
+    artists_df = filtered_df.groupby(["master_metadata_album_artist_name"]).agg(
         count=("ms_played", lambda x: (x >= 30000).sum()),
         total_ms_played=("ms_played", "sum")
     )
